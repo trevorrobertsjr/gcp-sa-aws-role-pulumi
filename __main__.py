c365a7537e55a35dbf3837e811a0713b58dedac8
Trevor Roberts Jr <trevor.roberts.jr@gmail.com>
2023-10-26 21:36:52 -0400

    code clean-up. ready to share

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
-"""An AWS and GCP Python Pulumi program"""
+"""Create an Amazon S3 bucket and allow a GCP Compute Instance to Access It"""
 
 import pulumi, json
-from pulumi_aws import iam
+from pulumi_aws import iam, s3
 from pulumi_gcp import serviceaccount, compute
 
 ### GCP
@@ -11,10 +11,7 @@ aws_service_access_sa = serviceaccount.Account("awsAccessServiceAccount",
     account_id="aws-service-access",
     display_name="AWS Service Access Service Account")
 
-# Store the GCP IAM Service Account UniqueId for creating the AWS IAM Role later
-unique_id = aws_service_access_sa.id
-
-# Create GCP compute resource with service account from #1 assigned
+# Create GCP compute resource with the custom service assigned
 aws_service_instance = compute.Instance("awsserviceaccess",
     machine_type="e2-micro",
     zone="us-east4-c",
@@ -34,29 +31,61 @@ aws_service_instance = compute.Instance("awsserviceaccess",
 #####################################################
 
 ### AWS
-# Create a function to use with the Pulumi Output apply() method call
-# This creates the policy used in the AWS trust relationship configuration for the IAM role
-def gcp_sa_assume_role_trust(unique_id):
-    return json.dumps({
-	"Version": "2012-10-17",
-	"Statement": [
-		{
-			"Effect": "Allow",
-			"Principal": {
-				"Federated": "accounts.google.com"
-			},
-			"Action": "sts:AssumeRoleWithWebIdentity",
-			"Condition": {
-				"StringEquals": {
-					"accounts.google.com:aud": unique_id
-				}
-			}
-		}
-	]
-    })
+# Create the S3 bucket for the GCP compute resources to access
+# NOTE: Do not use the force_destroy option in PRODUCTION
+#       I only used this setting because this is a demo environment.
+aws_bucket = s3.Bucket('gcp-sa-access-bucket', force_destroy=True)
 
-# Create IAM role and establish trust policy with Unique ID from Step 1
+# Create an IAM policy allowing read access to the bucket and its contents
+aws_iam_policy_bucket_read = iam.Policy("gcp-sa-access-bucket-read", policy=aws_bucket.id.apply(lambda name:
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "s3:GetObject",
+                    "s3:ListBucket",
+                    "s3:HeadBucket"
+
+                ],
+                "Resource": [
+                    f"arn:aws:s3:::{name}",
+                    f"arn:aws:s3:::{name}/*"
+                ]
+            }
+        ]
+    }
+))
+
+# Create IAM role and establish trust policy with the
+# GCP Service Account's Unique ID created earlier
+# Include the IAM policy arn as well.
 aws_s3_read_only_role = iam.Role("awsS3ReadRole",
-    assume_role_policy=unique_id.apply(gcp_sa_assume_role_trust)
+    assume_role_policy=aws_service_access_sa.unique_id.apply(lambda id:
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Federated": "accounts.google.com"
+                },
+                "Action": "sts:AssumeRoleWithWebIdentity",
+                "Condition": {
+                    "StringEquals": {
+                        "accounts.google.com:aud": id
+                    }
+                }
+            }
+        ]
+    }),
+    managed_policy_arns=[
+        aws_iam_policy_bucket_read.arn,
+    ]
 )
 #####################################################
+
+### Exports
+# Export the name of the GCP instance
+pulumi.export('gcp_instance_name', aws_service_instance.id)
